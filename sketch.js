244bb958c3a42e3b965d6450f95cdd44b3a69385
Sarakso1 <ozagarys@gmail.com>
2026-03-21 11:32:00 +0200

    made a very cool circle circling around a circle

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 function setup() {
   createCanvas(400, 400);
 
-  frameRate(2);
+  frameRate(100);
 }
 
 function draw() {
@@ -9,8 +9,18 @@ function draw() {
     Math.floor(Math.random() * 255),
     Math.floor(Math.random() * 255),
     Math.floor(Math.random() * 255));
-
   
+  var circle_path_radius = 100;
+  var circle_path_center_x = 200;
+  var circle_path_center_y = 200;
+  var time = millis() / 1000
+
+  var x = circle_path_radius*cos(time) + circle_path_center_x;
+  var y = circle_path_radius*sin(time) + circle_path_center_y;
+
+  background(220);
+  circle(x, y, 100)
+
 }
 
 
